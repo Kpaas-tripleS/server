c0bb54f652276b686fdb55c68134920c9d655fa9
ns-gyu <gyubin601@gmail.com>
2024-08-11 17:56:52 +0900

    feat: commit test

--- src/main/java/com/tripleS/server/ServerApplication.java
+++ src/main/java/com/tripleS/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.tripleS.server;
+/*package com.tripleS.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,4 +10,22 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+}*/
+package com.tripleS.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+
+		//이거 빼세요
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "local");
+
+		SpringApplication.run(ServerApplication.class, args);
+	}
 }
+
