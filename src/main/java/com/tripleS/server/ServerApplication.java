5e6b5a00cbdbf033937753bd2ca1d154127298a6
ns-gyu <gyubin601@gmail.com>
2024-08-15 17:46:04 +0900

    feat: 랜덤대결매칭 구현중

--- src/main/java/com/tripleS/server/ServerApplication.java
+++ src/main/java/com/tripleS/server/ServerApplication.java
@@ -1,31 +1,14 @@
-/*package com.tripleS.server;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
-
-}*/
 package com.tripleS.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
-
-		//이거 빼세요
-		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "local");
-
 		SpringApplication.run(ServerApplication.class, args);
 	}
+
 }
 
---
+++ src/main/java/com/tripleS/server/room/dto/request/MatchRequest.java
@@ -0,0 +1,25 @@
+package com.tripleS.server.room.dto.request;
+
+import java.time.LocalDateTime;
+
+public class MatchRequest {
+
+    private Long userId;
+    private LocalDateTime createTime;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+}
